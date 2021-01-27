5d1506b3b9ef8691b3780bed6c42632385cc21c7
unknown <fabiomyssionhills@gmail.com>
2021-01-27 14:23:49 -0300

    commit final

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           int numeroDevezes = 10;
+
+           for (int i=0; i < numeroDevezes; i++)
+           {
+               Console.WriteLine($"bem-vindo ou test .net {i}");
+           }
+
+           
+        }
+    }
+}
